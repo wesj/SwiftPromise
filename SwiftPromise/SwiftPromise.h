ffbfa319bdb5f02416913dc46c02504889fa9b97
Wes Johnston <wjohnston@mozilla.com>
2015-05-12 21:37:02 -0700

    Initial Commit

---
+++ SwiftPromise/SwiftPromise.h
@@ -0,0 +1,19 @@
+//
+//  SwiftPromise.h
+//  SwiftPromise
+//
+//  Created by Wes Johnston on 5/12/15.
+//  Copyright (c) 2015 Wes Johnston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftPromise.
+FOUNDATION_EXPORT double SwiftPromiseVersionNumber;
+
+//! Project version string for SwiftPromise.
+FOUNDATION_EXPORT const unsigned char SwiftPromiseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftPromise/PublicHeader.h>
+
+
